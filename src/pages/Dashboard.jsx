24e79dacef6539694d7854d32c244bf53b930e09
IshaanJhamb16 <jhambishaan@gmail.com>
2026-03-10 11:15:33 +0530

    Add files via upload

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <div style = {{backgroundColor:"navy",color:"white"}}>
+      <h2>Welcome to EcoTrack</h2>
+      <h4>Dashboard</h4>
+      <p>This is my Lab Mst Project</p>
+    </div>
+  );
+};
+
+export default Dashboard;
